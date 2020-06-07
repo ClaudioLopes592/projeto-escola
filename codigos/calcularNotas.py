d37b78fc7dcfc38c2421210e842ec261ac847e64
ClaudioLopes592 <claudioglopes1@gmail.com>
2020-06-07 20:24:51 -0300

    Implementando a soma das notas

--- codigos/calcularNotas.py
+++ codigos/calcularNotas.py
@@ -11,12 +11,15 @@ print('Nome do professor: {} '.format(professor))
 print('Matéria: {} '.format(materia))
 print('x.x.x.x.x.x.x.x.x.x N O T A S x.x.x.x.x.x.x.x.x.x')
 nota = 1
+soma = 0
 cont = 1
 opcao = 'S'
 while opcao == 'S':
     nota = float(input('Entre com a {}.ª nota deste aluno: '.format(cont)))
     cont += 1
     opcao = str(input('Deseja informar outra nota? [S|N]')).upper()
+    soma = soma + nota
 else:
     pass
+print('A soma apurada do aluno {} foi {}'.format(nome, soma))
 print('x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x')
