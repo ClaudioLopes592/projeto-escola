b57c7b5d830d7f225957991b4ecf3e10e22528b3
ClaudioLopes592 <claudioglopes1@gmail.com>
2020-06-08 16:27:06 -0300

    Iniciando uso de funções

--- codigos/calcularNotas.py
+++ codigos/calcularNotas.py
@@ -1,21 +1,29 @@
+def titulo(txt):
+    print('-' * 40)
+    print(txt)
+    print('-' * 40)
+
+def linha():
+    print('-' * 40)
+
 nome = str(input('Digite o nome do aluno: ')).upper()
 turma = str(input('Digite a turma: '))
 periodo = str(input('Digite o período: ')).upper()
 professor = str(input('Digite o nome do professor: ')).upper()
 materia = str(input('Digite a matéria: ')).upper()
-print('x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x')
-print('Nome do aluno: {} '.format(nome))
-print('Turma: {} '.format(turma))
-print('Período: {} '.format(periodo))
-print('Nome do professor: {} '.format(professor))
-print('Matéria: {} '.format(materia))
-print('x.x.x.x.x.x.x.x.x.x N O T A S x.x.x.x.x.x.x.x.x.x')
+titulo('            SISTEMA ESCOLAR          ')
+print(f'Nome do aluno: {nome}')
+print(f'Turma: {turma}')
+print(f'Período: {periodo}')
+print(f'Nome do professor: {professor}')
+print(f'Matéria: {materia}')
+titulo('            N O T A S              ')
 notas = []
 soma = 0
 cont = 1
 opcao = 'S'
 while opcao == 'S':
-    nota = float(input('Entre com a {}.ª nota deste aluno: '.format(cont)))
+    nota = float(input(f'Entre com a {cont}.ª nota deste aluno: '))
     notas.append(nota)
     quant = len(notas)
     soma += nota
@@ -24,4 +32,4 @@ while opcao == 'S':
 else:
     pass
 print('A média das notas apuradas do aluno {} é {:.1f}'.format(nome,soma/quant))
-print('x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x=x')
+linha()
